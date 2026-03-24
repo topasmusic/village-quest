437851b8ab419b299a7f9987e5da09acc403a7ea
topasmusic <topasmusic@users.noreply.github.com>
2026-03-24 22:16:52 +0100

    Release 1.19.3 furnace quest hook fixes

--- src/main/java/de/quest/mixin/AbstractFurnaceScreenHandlerMixin.java
+++ src/main/java/de/quest/mixin/AbstractFurnaceScreenHandlerMixin.java
@@ -3,33 +3,33 @@ package de.quest.mixin;
 import de.quest.quest.daily.DailyQuestService;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.slot.FurnaceOutputSlot;
+import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FurnaceOutputSlot.class)
-public abstract class FurnaceOutputSlotMixin {
-    @Shadow
-    @Final
-    private PlayerEntity player;
+@Mixin(AbstractFurnaceScreenHandler.class)
+public abstract class AbstractFurnaceScreenHandlerMixin {
+    private static final int VILLAGE_QUEST$OUTPUT_SLOT_INDEX = 2;
 
-    @Inject(method = "takeStack", at = @At("RETURN"))
-    private void villageQuest$trackFurnaceOutput(int amount, CallbackInfoReturnable<ItemStack> cir) {
-        if (!(this.player instanceof ServerPlayerEntity serverPlayer)) {
+    @Inject(method = "quickMove", at = @At("RETURN"))
+    private void villageQuest$trackQuickMoveOutput(PlayerEntity player, int slot, CallbackInfoReturnable<ItemStack> cir) {
+        if (slot != VILLAGE_QUEST$OUTPUT_SLOT_INDEX) {
             return;
         }
-        ServerWorld serverWorld = (ServerWorld) serverPlayer.getEntityWorld();
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) {
+            return;
+        }
+
         ItemStack stack = cir.getReturnValue();
         if (stack == null || stack.isEmpty()) {
             return;
         }
 
+        ServerWorld serverWorld = (ServerWorld) serverPlayer.getEntityWorld();
         DailyQuestService.onFurnaceOutput(serverWorld, serverPlayer, stack);
     }
 }
